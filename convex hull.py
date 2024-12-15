8a53b2335f44815992cddb5264c59ddc3d7e26af
elenshahbazyan <elenshahbazyan2002@gmail.com>
2024-12-15 21:47:37 +0400

    Update convex hull.py

--- convex hull.py
+++ convex hull.py
@@ -1,5 +1,3 @@
-import math
-
 def orientation(p, q, r):
     val = (q[1] - p[1]) * (r[0] - q[0]) - (q[0] - p[0]) * (r[1] - q[1])
     if val == 0:
@@ -9,26 +7,32 @@ def orientation(p, q, r):
     else:
         return 2
 
-def polar_angle(p0, p):
-    return math.atan2(p[1] - p0[1], p[0] - p0[0])
+def convex_hull(points):
+    n = len(points)
+    if n < 3:
+        return []  # Convex hull is not possible with less than 3 points
 
-def graham_scan(points):
-    p0 = min(points, key=lambda p: (p[1], p[0]))
-    points.remove(p0)
-    points.sort(key=lambda p: polar_angle(p0, p))
-    
-    hull = [p0, points[0], points[1]]
-    
-    for i in range(2, len(points)):
-        while len(hull) > 1 and orientation(hull[-2], hull[-1], points[i]) != 2:
-            hull.pop()
-        hull.append(points[i])
-    
+    hull = []
+
+    l = min(range(n), key=lambda i: points[i][0])  # Leftmost point
+    p = l
+    while True:
+        hull.append(points[p])
+        q = (p + 1) % n
+        for i in range(n):
+            if orientation(points[p], points[i], points[q]) == 2:
+                q = i
+        p = q
+        if p == l:
+            break
+
     return hull
 
-points = [(0, 3), (2, 2), (1, 1), (2, 1), (3, 0), (0, 0), (3, 3), (1, 2)]
-convex_hull = graham_scan(points)
-print("Convex Hull:", convex_hull)
+points = [(0, 0), (1, 1), (2, 2), (2, 0), (3, 1), (3, 3), (0, 3)]
+hull = convex_hull(points)
+
+print("Convex Hull:", hull)
+
 
 
 
