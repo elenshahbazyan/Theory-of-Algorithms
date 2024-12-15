7d9234e5b60b2f92223a38d5cec13a71e59f89e5
elenshahbazyan <elenshahbazyan2002@gmail.com>
2024-12-15 21:21:09 +0400

    Update linear search.py

--- linear search.py
+++ linear search.py
@@ -1,24 +1,16 @@
-def binary_search(arr, search):
-    arr.sort()
-    left = 0
-    right = len(arr) - 1
-
-    while left <= right:
-        mid = (left + right) // 2 
-        if arr[mid] == search:
-            return mid
-        elif arr[mid] > search:
-            right = mid - 1
-        else:
-            left = mid + 1
+def linear_search(arr, search):
+    for i in range(len(arr)):
+        if arr[i] == search:
+            return i
     return -1
 
 arr = list(map(int, input("Enter the elements: ").split()))
 search = int(input("Search for: "))
 
-A = binary_search(arr, search)
+A = linear_search(arr, search)
 
-print("Found at index:" ,A)
+print("Found at index:", A)
+
 
 
 
