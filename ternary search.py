a243121f2fe2f563b1c6cdc4bdad7e32e714d29e
elenshahbazyan <elenshahbazyan2002@gmail.com>
2024-12-15 21:17:54 +0400

    Update ternary search.py

--- ternary search.py
+++ ternary search.py
@@ -1,24 +1,32 @@
-import math
+def ternary_search(arr, search):
+    arr.sort()
+    left = 0
+    right = len(arr) - 1
 
-def jump_search(arr, search):
-    n = len(arr)
-    step = int(math.sqrt(n))
-    jump_from = 0
-    while arr[min(step, n) - 1] < search:
-        jump_from = step
-        step += int(math.sqrt(n))
-        if jump_from >= n:
-            return -1
-    for i in range(jump_from, min(step, n)):
-        if arr[i] == search:
-            return i
-    return -1
+    while left <= right:
+        mid1 = left + (right - left) // 3
+        mid2 = right - (right - left) // 3
+        
+        if arr[mid1] == search:
+            return f"Element {search} found at index {mid1}"
+        elif arr[mid2] == search:
+            return f"Element {search} found at index {mid2}"
+        if search < arr[mid1]:
+            right = mid1 - 1
+        elif search > arr[mid2]:
+            left = mid2 + 1
+        else:
+            left = mid1 + 1
+            right = mid2 - 1
+    
+    return f"Element {search} not found in the list"
 
 arr = list(map(int, input("Enter the elements: ").split()))
-search = int(input("Search for: "))
+search = int(input("search for: "))
 
-A = jump_search(arr, search)
-print(A)
+A = ternary_search(arr, search)
+
+print(" Found at index:",A)
 
 
 
